be1c68ac9221bb8246a62d2aab4ed7c7d3644397
Elliot Glaysher <erg@chromium.org>
2015-03-25 16:50:38 -0700

    Move from calculating hsvs at startup to hard coded colors.

--- dark-forest-theme.el
+++ dark-forest-theme.el
@@ -4,179 +4,71 @@ text on dark background font-lock colors, slightly modified, and
 then applied to all of emacs faces. Inspired by Bob Hauser's Black
 Forest theme.
 
-The value component of each color has been normalized to .10/1
-depending on darkness/lightness. Valid saturation values are
-.35/.65. Hues attempt to be multiples of 25.")
+We start off with the following base colors as the 'medium' variants:
 
-;; Original colors in HSV:
-;; FG:            51/45/93
-;; string/red:    17/52/98
-;; builtin/blue:  215/21/86
-;; type/green:    120/39/98  (#98fb98)
-;; keyword/cyan:  180/100/100
+  red     '(  0   45  90)
+  orange  '( 17   52 100)
+  yellow  '( 51   45  93)
+  green   '(120   39  98)
+  cyan    '(180   60  90)
+  blue    '(210   46  95)
+  violet  '(280   45  90)
 
+And then performed various hsv shifts across this pallet for
+light, dark, bold, etc variants. (The hsv calculations used to be
+done at build time; it's only recently that I considered the base
+pallet to be complete enough that I started hard coding the hex
+into this file.)")
 
-;; Stolen from hexrgb.el until I've settled on the colors. Then I'll remove
-;; these and hard code the constants.
-(eval-when-compile (require 'cl)) ;; case
-(defun hexrgb-hsv-to-rgb (hue saturation value)
-  "Convert HUE, SATURATION, VALUE components to RGB (red, green, blue).
-Each input component is 0.0 to 1.0, inclusive.
-Returns a list of RGB components of value 0.0 to 1.0, inclusive."
-  (let (red green blue int-hue fract pp qq tt ww)
-    (if (hexrgb-approx-equal 0.0 saturation)
-        (setq red    value
-              green  value
-              blue   value)             ; Gray
-      (setq hue      (* hue 6.0)        ; Sectors: 0 to 5
-            int-hue  (floor hue)
-            fract    (- hue int-hue)
-            pp       (* value (- 1 saturation))
-            qq       (* value (- 1 (* saturation fract)))
-            ww       (* value (- 1 (* saturation (- 1 (- hue int-hue))))))
-      (case int-hue
-        ((0 6) (setq red    value
-                     green  ww
-                     blue   pp))
-        (1 (setq red    qq
-                 green  value
-                 blue   pp))
-        (2 (setq red    pp
-                 green  value
-                 blue   ww))
-        (3 (setq red    pp
-                 green  qq
-                 blue   value))
-        (4 (setq red    ww
-                 green  pp
-                 blue   value))
-        (otherwise (setq red    value
-                         green  pp
-                         blue   qq))))
-    (list red green blue)))
+(let* (
+      (df-fg        "#EE13DE0182F1")
+      (df-fg-white  "#EE13ED01E6EF") ; gray-93
 
-;; Color "components" would be better in the name than color "value"
-;; but this name follows the Emacs tradition (e.g. `x-color-values',
-;; 'ps-color-values', `ps-e-x-color-values').
-(defun hexrgb-color-values-to-hex (components)
-  "Convert list of rgb color COMPONENTS to a hex string, #XXXXXXXXXXXX.
-Each X in the string is a hexadecimal digit.
-Input COMPONENTS is as for the output of `x-color-values'."
-;; Just hard-code 4 as the number of hex digits, since `x-color-values'
-;; seems to produce appropriate integer values for `4'.
-  (concat "#" (hexrgb-int-to-hex (nth 0 components) 4) ; red
-          (hexrgb-int-to-hex (nth 1 components) 4) ; green
-          (hexrgb-int-to-hex (nth 2 components) 4))) ; blue
+      ;; Our default grays have a subtle tint of amber in them. (hue=51, sat=3)
+      (df-gray-80    "#CCCCCBE0C6A7")
+      (df-gray       "#B332B264ADD2")
+      (df-gray-60    "#999998E894FD")
+      (df-dim-gray   "#733272AE6FBE")
+      (df-gray-30    "#4CCC4C744A7E")
+      (df-gray-15    "#2666263A253F")
 
-;; Inspired by Elisp Info manual, node "Comparison of Numbers".
-(defun hexrgb-approx-equal (x y &optional rfuzz afuzz)
-  "Return non-nil if numbers X and Y are approximately equal.
-RFUZZ is a relative fuzz factor.  AFUZZ is an absolute fuzz factor.
-RFUZZ defaults to 1.0e-8.  AFUZZ defaults to (/ RFUZZ 10).
-RFUZZ and AFUZZ are converted to their absolute values.
-The algorithm is:
- (< (abs (- X Y)) (+ AFUZZ (* RFUZZ (+ (abs X) (abs Y)))))."
-  (setq rfuzz  (or rfuzz 1.0e-8)
-        rfuzz  (abs rfuzz)
-        afuzz  (or afuzz (/ rfuzz 10))
-        afuzz  (abs afuzz))
-  (< (abs (- x y)) (+ afuzz (* rfuzz (+ (abs x) (abs y))))))
+      ;; The super-superlight colors. (hsv shift -25, -5)
+      (df-ssl-blue  "#B603CE34E665")
 
-;; Originally, I used the code from `int-to-hex-string' in `float.el'.
-;; This version is thanks to Juri Linkov <juri@jurta.org>.
-;;
-(defun hexrgb-int-to-hex (int &optional nb-digits)
-  "Convert integer argument INT to a #XXXXXXXXXXXX format hex string.
-Each X in the output string is a hexadecimal digit.
-NB-DIGITS is the number of hex digits.  If INT is too large to be
-represented with NB-DIGITS, then the result is truncated from the
-left.  So, for example, INT=256 and NB-DIGITS=2 returns \"00\", since
-the hex equivalent of 256 decimal is 100, which is more than 2 digits."
-  (setq nb-digits  (or nb-digits 4))
-  (substring (format (concat "%0" (int-to-string nb-digits) "X") int) (- nb-digits)))
+      ;; The superlight colors. (hsv shift -15, 5)
+      (df-sl-blue    "#B0A3D851FFFF")
+      (df-sl-orange  "#FFFFBC1DA147")
 
+      ;; The light set of colors. (hsv shift -5, 5)
+      (df-l-red    "#F33291EA91EA")
+      (df-l-orange "#FFFFA9C487AD")
+      (df-l-yellow "#FAE0EBD29686")
+      (df-l-green  "#A8F5FFFFA8F5")
+      (df-l-cyan   "#6D70F332F332")
+      (df-l-blue   "#9709CB84FFFF")
 
-(defun hexrgb-hsv-to-hex (hue saturation value)
-  "Return the hex RBG color string for inputs HUE, SATURATION, VALUE.
-The inputs are each in the range 0 to 1.
-The output string is of the form \"#RRRRGGGGBBBB\"."
-  (hexrgb-color-values-to-hex
-   (mapcar (lambda (x) (floor (* x 65535.0))) (hexrgb-hsv-to-rgb hue saturation value))))
+      ;; "Medium" colors. (hsv shift 0, 0)
+      (df-m-red    "#E6657EB77EB7")
+      (df-m-orange "#FFFFA0987AE0")
+      (df-m-yellow "#EE13DE0182F1")
+      (df-m-green  "#9908FAE09908")
+      (df-m-cyan   "#5C28E665E665")
+      (df-m-blue   "#8353BB42F332")
+      (df-m-violet "#C3D67EB7E665")
 
-(defun dfb-clamp (num min-num max-num)
-  (min (max num min-num) max-num))
-
-(defun dfb-hsv (base saturation-offset value-offset)
-  "Given the base (HUE SATURATION VALUE), applies saturation-offset
-and value and returns a color hex string."
-  (hexrgb-hsv-to-hex (/ (nth 0 base) 360.0)
-                     (/ (dfb-clamp (+ (nth 1 base) saturation-offset) 0 100)
-                        100.0)
-                     (/ (dfb-clamp (+ (nth 2 base) value-offset) 0 100)
-                        100.0)))
-
-(defun dfb-gray (lum)
-  "Builds our grays that have just the slightest hint of amber in therm."
-  (hexrgb-hsv-to-hex (/ 51.0 360.0)
-                     (/ 3.0 100.0)
-                     (/ lum 100.0)))
-
-(let* (;; First are all the base colors. These are hue/saturation/value
-       ;; triples and are mostly based on the default font-lock. Integer in terms
-       ;; of 360 degrees for hue / 100 percent for saturation and value.
-       (dfb-red        '(  0   45  90))
-       (dfb-orange     '( 17   52 100))
-       (dfb-yellow     '( 51   45  93))
-       (dfb-green      '(120   39  98))
-       (dfb-cyan       '(180   60  90))
-       (dfb-blue       '(210   46  95))
-       (dfb-violet     '(280   45  90))
-
-      (df-fg        (dfb-hsv dfb-yellow 0 0))
-      (df-fg-white  (dfb-gray 93))
-
-      (df-gray-80    (dfb-gray 80))
-      (df-gray       (dfb-gray 70))
-      (df-gray-60    (dfb-gray 60))
-      (df-dim-gray   (dfb-gray 45))
-      (df-gray-30    (dfb-gray 30))
-      (df-gray-15    (dfb-gray 15))
-
-      (df-ssl-blue  (dfb-hsv dfb-blue   -25 -5))
-
-      ;; The superlight colors.
-      (df-sl-blue    (dfb-hsv dfb-blue   -15 5))
-      (df-sl-orange  (dfb-hsv dfb-orange -15 5))
-
-      ;; The light set of colors.
-      (df-l-red    (dfb-hsv dfb-red    -5 5))
-      (df-l-orange (dfb-hsv dfb-orange -5 5))
-      (df-l-yellow (dfb-hsv dfb-yellow -5 5))
-      (df-l-green  (dfb-hsv dfb-green  -5 5))
-      (df-l-cyan   (dfb-hsv dfb-cyan   -5 5))
-      (df-l-blue   (dfb-hsv dfb-blue   -5 5))
-
-      ;; "Medium" colors.
-      (df-m-red    (dfb-hsv dfb-red 0 0))
-      (df-m-orange (dfb-hsv dfb-orange 0 0))
-      (df-m-yellow (dfb-hsv dfb-yellow 0 0))
-      (df-m-green  (dfb-hsv dfb-green 0 0))
-      (df-m-cyan   (dfb-hsv dfb-cyan 0 0))
-      (df-m-blue   (dfb-hsv dfb-blue 0 0))
-      (df-m-violet (dfb-hsv dfb-violet 0 0))
-
-      ;; Bolder colors.
-      (df-b-yellow (dfb-hsv dfb-yellow 20 10))
+      ;; Bolder colors. (hsv shift 20, 10)
+      (df-b-yellow "#FFFFE7095999")
 
       ;; "Dark" colors, which have saturation 55 and value of 80. Mainly used
-      ;; for flyspell checking. Significantly darker than any of the above.
-      (df-d-red    (dfb-hsv dfb-red 15 -15))
-      (df-d-orange (dfb-hsv dfb-orange 15 -15))
-      (df-d-yellow (dfb-hsv dfb-yellow 15 -15))
-      (df-d-green (dfb-hsv dfb-green 15 -15))
-      (df-d-cyan (dfb-hsv dfb-cyan 15 -15))
-      (df-d-blue (dfb-hsv dfb-blue 15 -15))
-      (df-d-violet (dfb-hsv dfb-violet 15 -15))
+      ;; for flyspell checking. Significantly darker than any of the
+      ;; above. (hsv shift 15, -15)
+      (df-d-red    "#BFFF4CCC4CCC")
+      (df-d-orange "#D998711D47CE")
+      (df-d-yellow "#C7ADB5B44FDE")
+      (df-d-green "#61BDD47A61BD")
+      (df-d-cyan "#2FFFBFFFBFFF")
+      (df-d-blue "#4FDE8E55CCCC")
+      (df-d-violet "#99994CCCBFFF")
 
       ;; "Black" background colors. Our "gray" has the slightest hint of amber
       ;; tint, but this is pure black/gray.
